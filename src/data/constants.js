6973561269b347e542321e527b0086bbd3ef1d58
preenasultana <reenasultanapattan@gmail.com>
2024-08-04 00:02:07 +0530

    completed

---
+++ src/data/constants.js
@@ -0,0 +1,229 @@
+export const Bio = {
+  name: "PATTAN REENA SULTANA",
+  roles: [
+    "Web Developer",
+  ],
+  description:
+"I am a final year B.Tech student in Computer Science and Engineering at Narayana Engineering College, Nellore. With good skills in web development and I am passionate about leveraging technology to solve real-world problems.",
+  github: "https://github.com/preenasultana",
+  resume:
+    "https://firebasestorage.googleapis.com/v0/b/portfolio-e8931.appspot.com/o/REENARESUME.pdf?alt=media&token=54be90fc-77d5-4962-8348-14abbd8f102a",
+  linkedin: "https://www.linkedin.com/in/reena-sultana-pattan-7a1251231/",
+};
+
+export const skills = [
+  {
+    title: "Languages",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://img.icons8.com/?size=100&id=UpJmIaNZwNyq&format=png&color=000000",
+      },
+      {
+        name: "C",
+        image:
+        "https://img.icons8.com/?size=100&id=25423&format=png&color=000000",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Libraries",
+    skills: [
+      {
+        name: "Python Libraries",
+        image: "https://img.icons8.com/?size=100&id=UpJmIaNZwNyq&format=png&color=000000",
+      },
+    ],
+  },
+  {
+    title: "Web dev tools",
+    skills: [
+      {
+        name: "Node js",
+        image:
+          "https://img.icons8.com/?size=100&id=hsPbhkOH4FMe&format=png&color=000000",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://img.icons8.com/?size=100&id=9OGIyU8hrxW5&format=png&color=000000",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://img.icons8.com/?size=100&id=52539&format=png&color=000000",
+      },
+    ],
+  },
+  {
+    title: "Cloud and Databases",
+    skills: [
+      {
+        name: "Aws",
+        image:
+          "https://img.icons8.com/?size=100&id=33039&format=png&color=000000",
+      },
+      {
+        name: "MySql",
+        image:
+          "https://img.icons8.com/?size=100&id=19672&format=png&color=000000",
+      }  
+    ]
+  },
+]
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-e8931.appspot.com/o/aicte.png?alt=media&token=51901815-ee89-4ee0-9381-0b8033c50ebe",
+    role: "Data Science Master Virtual Internship",
+    company: "AICTE - EDUSKILLS",
+    date: "April  - June 2024",
+    desc: "Evaluated the machine learning models and techniques",
+    skills: [
+      "Python",
+      "Python Libraries like Keras , Tensorflow , Matplotlib, Numpy, Pandas."
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/portfolio-e8931.appspot.com/o/DOC-20240618-WA0008..pdf?alt=media&token=3ca10201-9885-4f12-9c9d-47f940e251c2",
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-e8931.appspot.com/o/coincent.jpg?alt=media&token=fef1d921-a0e8-4716-b69a-4cf6ccb5cdc2",
+    role: "Machine learning Intern",
+    company: "Coincent",
+    date: "May - June 2023",
+    desc: "Learned techniques for data processing and feature Engineering.",
+    skills: [
+      "Python",
+      "Python Libraries like tensorflow, keras, Numpy, Pandas, Matplotlib."
+      
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/portfolio-e8931.appspot.com/o/PATTAN_May_2023_internship_44431.pdf?alt=media&token=7f670751-9556-40db-aa77-4c67993bbfe8",
+  },
+  {
+    id: 3,
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-e8931.appspot.com/o/bharat.png?alt=media&token=8a1ffcbd-3a06-4805-950b-7380f1381f5b",
+    role: "Web Development Intern",
+    company: "Bharat Internship",
+    date: "July - Present",
+    desc: "Learned about technologies used in web development and worked on building the projects.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript"
+    ],
+    doc:""
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-e8931.appspot.com/o/narayana.png?alt=media&token=6992f153-bf9f-4053-bf42-7f746878085a",
+    school: "Narayana Engineering College, Nellore",
+    date: "Nov 2021 - Apr 2025",
+    grade: "9.2 CGPA",
+    desc: "I am currently pursuing final year of B.Tech in computer science and engineering.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-e8931.appspot.com/o/narayana.png?alt=media&token=6992f153-bf9f-4053-bf42-7f746878085a",
+    school: "Narayana Jr. College, Nellore",
+    date: "Apr 2019 - Apr 2021",
+    grade: "96.6%",
+    desc: "I completed my class 12 high school education at Narayana jr. college, Nellore.",
+    degree: "Intermediate (+12)",
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-e8931.appspot.com/o/images.png?alt=media&token=193a6af7-53fd-4f7e-af0e-3805245e82b3",
+    school: "Narayana High School, Nellore",
+    date: "Apr 2018 - Apr 2019",
+    grade: "98.0%",
+    desc: "I completed my class 10 education at Narayana High School, Nellore.",
+    degree: "Secondary School Certificate(SSC)",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Fashion MNIST",
+    date: "",
+    description:
+      "Developed a machine learning model to classify images of clothing items from the Fashion MNIST dataset.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-e8931.appspot.com/o/fashion_mnist-3.0.1.png?alt=media&token=32ec9574-a274-4c6a-8a7e-1729114d2c7e",
+    tags: [
+      "Jupyter",
+      "Python",
+      "Python Libraries"
+    ],
+    category: "machine learning",
+    github: "https://github.com/preenasultana/Fashion-MNIST-Data-classification",
+    machinelearning: "",
+  },
+  {
+    id: 1,
+    title: "Basic Core Banking System",
+    date: "",
+    description:
+      " Designed and developed a basic core banking system that enables fundamental banking operations.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-e8931.appspot.com/o/bank2.jpg?alt=media&token=7cde1111-3a61-48bc-9a15-c245513ce7e6",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MySql",
+      "php",
+      "XAMPP"
+    ],
+    category: "web app",
+    github: "https://github.com/preenasultana/Basic-Core-Banking-System",
+    webapp: "http://localhost/Basic-Core-Banking-System/",
+  },
+  {
+    id: 2,
+    title: "Real Estate",
+    date: "",
+    description:
+      " Created a dynamic real estate listing website featuring property, services and updates.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-e8931.appspot.com/o/hero-banner.jpg?alt=media&token=713bcac8-50cd-426d-9381-fcf5acf968e2",
+    tags: ["HTML","CSS","JavaScript"],
+    category: "web app",
+    github: "https://github.com/preenasultana/realestate",
+    webapp: "",
+  },
+  {
+  id: 3,
+  title: "Netflix Clone",
+  date: "",
+  description:
+    "Created a clone based on Netflix",
+  image:
+    "https://firebasestorage.googleapis.com/v0/b/portfolio-e8931.appspot.com/o/images%20(1).png?alt=media&token=059eafa1-19f9-48a0-8b68-eca247f1106c",
+  tags: ["HTML","CSS"],
+  category: "web app",
+  github: "https://github.com/preenasultana/NetflixClone",
+  webapp:"",
+  },
+];
+
